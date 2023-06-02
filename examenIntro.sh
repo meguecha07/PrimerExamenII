f9210634b244c3e1ec25da97a9effccd1f8611ca
Melissa Guerra <melissaguerrachamorro@gmail.com>
2023-06-02 12:13:25 -0500

    mi primer commit de examen intro

---
+++ examenIntro.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mv memorias/ram.txt memorias/memoriaPrincipal/ram.txt
+mv memorias/rom.txt memorias/memoriaPrincipal/rom.txt
+mv memorias/registros.txt memorias/memoriaPrincipal/registros.txt
+mv memorias/cache.txt memorias/memoriaPrincipal/cache.txt
+mv memorias/ssd.txt memorias/memoriaSecundaria/ssd.txt
+mv memorias/dvd.txt memorias/memoriaSecundaria/dvd.txt
+mv memorias/discoExtraible.txt memorias/memoriaSecundaria/discoExtraible.txt
